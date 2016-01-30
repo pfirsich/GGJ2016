b6b3a36e5931b9d8a63cc5b9db90f1d596b442d9
rubensparka <ruben-sparka@gmx.de>
2016-01-30 02:03:52 +0100

    messege here

--- const.lua
+++ const.lua
@@ -1,5 +1,5 @@
 const = {
 	SIM_DT = 1/60.0,
-	PLAYER_SPEED = 100; --maximal speed of player character
+	PLAYER_SPEED = 250; --maximal speed of player character
 	GP_DEADZONE = 0.2; --gamepad deadzone
 }
