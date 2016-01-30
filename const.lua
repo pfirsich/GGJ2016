fe72a910779885a0aa71fac1b02dc4763f24f110
rubensparka <ruben-sparka@gmx.de>
2016-01-30 01:32:12 +0100

    player controls
    stuff

--- const.lua
+++ const.lua
@@ -1,3 +1,5 @@
 const = {
 	SIM_DT = 1/60.0,
+	PLAYER_SPEED = 100; --maximal speed of player character
+	GP_DEADZONE = 0.2; --gamepad deadzone
 }
--- player.lua
+++ player.lua
@@ -8,6 +8,8 @@ function getPlayerController_Gamepad(joystick)
 	local ctrl = {}
 	ctrl.moveX = input.floatInput_fromGamepad(joystick, "leftx")
 	ctrl.moveY = input.floatInput_fromGamepad(joystick, "lefty")
+	ctrl.angleX = input.floatInput_fromGamepad(joystick, "rightx")
+	ctrl.angleY = input.floatInput_fromGamepad(joystick, "righty")
 	return ctrl
 end
 
@@ -26,7 +28,22 @@ end
 function players.update()
 	for i, player in ipairs(players) do
 		input.updateController(player.controller)
+
+	--**character controls**
+		--velocity
+	player.velocity = vmul({player.controller.moveX.state, player.controller.moveY.state}, const.PLAYER_SPEED)
+	if vnorm({player.controller.moveX.state, player.controller.moveY.state}) < const.GP_DEADZONE then --deadzone
+		player.velocity = vmul(player.velocity, 0.8)
 	end
+	player.position = vadd(player.position, vmul(player.velocity, const.SIM_DT))
+
+		--orientation
+	if vnorm({player.controller.angleX.state, player.controller.angleY.state}) > const.GP_DEADZONE * 2 then --deadzone, higher than above because orientation with analog stick is weird
+		player.angle = vangle({player.controller.angleX.state, player.controller.angleY.state}) + math.pi/2
+	end
+
+	end
+
 end
 
 function players.draw()
